c59c6a8b9638184509aebf27a19688cf0e763a4b
Zac Baston <zbaston@gmail.com>
2017-05-10 09:54:52 -0400

    fix sidebar location, remove old exercise sidebar component

--- src/App/index.js
+++ src/App/index.js
@@ -19,7 +19,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="content">
+      <div className="content App">
         <Header />
         <ClockContainer />
         <Sidebar />
